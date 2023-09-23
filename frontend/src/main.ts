347edc8a8f4bc52391d1c49b1ab4811e3c6d1a81
wwd <1292042074@qq.com>
2023-09-23 14:20:05 +0800

    init

--- frontend/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import Antd from "ant-design-vue";
-import "ant-design-vue/dist/antd.css";
-
-createApp(App).use(Antd).use(router).mount("#app");
